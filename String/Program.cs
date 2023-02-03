773e33e12678ac4cac433c46eda42e337d294604
Daria <kukuruzadasha86@gmail.com>
2023-02-03 19:04:27 +0200

    all

--- String/Program.cs
+++ String/Program.cs
@@ -16,12 +16,13 @@ namespace String
                 Console.Write("Введите задание, которое хотите проверить (от 8 до 15, для выхода нажмите 0) -> ");
                 string test = Console.ReadLine();
 
-                if(test == "0")
+                if (test == "0")
                 {
                     exit = false;
                 }
                 if (test == "8")
                 {
+                    Console.Clear();
                     StringBuilder s = new StringBuilder("Реализовать удаление из строки символа с заданным номером");
                     Console.WriteLine(s);
 
@@ -33,6 +34,7 @@ namespace String
                 }
                 if (test == "9")
                 {
+                    Console.Clear();
                     string str = "Реаллизовать удаление из строки все вхождения в нее задонного символа";
                     StringBuilder s = new StringBuilder(str);
                     Console.WriteLine(str);
@@ -55,6 +57,7 @@ namespace String
                 }
                 if (test == "10")
                 {
+                    Console.Clear();
                     string str = "Написать метод, который вставляет в строку указанную позицию (заданный символ)";
 
                     Console.WriteLine(str);
@@ -70,6 +73,7 @@ namespace String
                 }
                 if (test == "11")
                 {
+                    Console.Clear();
                     Console.Write("Введите слово -> ");
                     string str = Console.ReadLine();
 
@@ -100,6 +104,7 @@ namespace String
                 }
                 if (test == "12")
                 {
+                    Console.Clear();
                     Console.Write("Введите предложение -> ");
                     string[] str = Console.ReadLine().Split(new char[] { ' ', '\n', '\t', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
 
@@ -107,6 +112,7 @@ namespace String
                 }
                 if (test == "13")
                 {
+                    Console.Clear();
                     Console.Write("Введите предложение -> ");
                     string str = Console.ReadLine();
 
@@ -119,11 +125,84 @@ namespace String
                     string modified = str.Replace(finde, change);
                     Console.WriteLine($"Предложение после замены -> {modified}");
                 }
-                if(test == "14")
+                if (test == "14")
                 {
+                    Console.Clear();
+                    Console.Write("Введите предложение -> ");
+
+                    string[] str = Console.ReadLine().Split(new char[] { ' ', '\n', '\t', '.', ',' }, StringSplitOptions.None);
+
+                    Console.Write("Введите номер позиции, для перестановки местами соседних элементов -> ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    if (num == 1 || num == str.Length)
+                    {
+                        Console.Write("Ошибка, соседние слова невозможно поменять местами");
+                        Exit(0);
+                    }
+                    string addContainer;
+                    Console.Write($"Видоизмененное предложение -> ");
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        if (str[i] == str[num - 1])
+                        {
+                            addContainer = str[i - 1];
+                            str[i - 1] = str[i + 1];
+                            str[i + 1] = addContainer;
+                            break;
+                        }
+                    }
+
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        Console.Write(str[i]);
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                }
+                if (test == "15")
+                {
+                    Console.Clear();
+                    Console.Clear();
+                    Console.Write("Введите предложение -> ");
+                    string[] str = Console.ReadLine().Split(new char[] { ' ', '\n', '\t', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    char[][] mass = new char[str.Length][];
+                    for (int i = 0; i < mass.Length; i++)
+                        mass[i] = new char[str[i].Length];
+
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        int j = 0;
+                        foreach (char ch in str[i])
+                        {
+                            mass[i][j] = ch;
+                            j++;
+                        }
+                    }
+
+                    int count = 0;
+                    for (int i = 0; i < mass.Length; i++)
+                    {
+                        for(int j = 0; j < mass[i].Length; j++)
+                        {
+                            if(j == mass[i].Length - 1 && (mass[i][j] == 'и' || mass[i][j] == 'ё' || mass[i][j] == 'е'
+                                || mass[i][j] == 'а'|| mass[i][j] == 'о'|| mass[i][j] == 'у'|| mass[i][j] == 'ы'
+                                || mass[i][j] == 'э'|| mass[i][j] == 'ю'|| mass[i][j] == 'я'))
+                            {
+                                count++;
+                            }
+                        }
+                    }
+
+                    Console.WriteLine($"Количество слов, которые заканчиваются на гласную букву -> {count}");
 
                 }
             }
+        }
+
+        private static void Exit(int v)
+        {
+            throw new NotImplementedException();
         }
     }
 }
